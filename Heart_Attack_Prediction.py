07e0fd81c36616923bab89d69c0d8865f310151a
anonymous23at <anonymoushackerat23@gmail.com>
2024-07-08 16:44:48 +0530

    heart attack prediction

--- Heart_Attack_Prediction.py
+++ Heart_Attack_Prediction.py
@@ -7,6 +7,7 @@ import datetime
 import tkinter as tk
 from tkinter import ttk
 import os
+from tkinter import messagebox
 
 import matplotlib
 
@@ -16,6 +17,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import numpy as np
 import matplotlib.pyplot as plt
+
+from backend import *
 
 background="#f0ddd5"
 framebg="#62a7ff"
@@ -27,14 +30,208 @@ root.geometry("1450x730+60+80")
 root.resizable(False,False)
 root.config(bg=background)
 
+# Analysis
+def analysis():
+    name = Name.get()
+    D1=Date.get()
+    today=datetime.date.today()
+    A=today.year-DOB.get()  # Check this line
+
+    try:
+        B=selection()
+    except:
+        messagebox.showerror("missing","Please select gender!!")  
+        return
+    
+    try:
+        F=selection2()
+    except:
+        messagebox.showerror("missing","Please select fbs!!")  
+        return
+    
+    try:
+        I=selection3()
+    except:
+        messagebox.showerror("missing","Please select exang!!")  
+        return
+    
+    try:
+        C= int(selection4())
+    except:
+        messagebox.showerror("missing","Please select cp!!")  
+        return
+    
+    try:
+        G= int(restecg_combobox.get())
+    except:
+        messagebox.showerror("missing","Please select restecg!!")  
+        return
+    
+    try:
+        K= int(selection5())
+    except:
+        messagebox.showerror("missing","Please select slope!!")  
+        return
+    
+    try:
+        L= int(ca_combobox.get())
+    except:
+        messagebox.showerror("missing","Please select ca!!")  
+        return
+    
+        
+    try:
+        M= int(thal_combobox.get())
+    except:
+        messagebox.showerror("missing","Please select thal!!")  
+        return
+    
+    try:
+        D= int(trestbps.get())
+        E= int(chol.get())
+        H= int(thalach.get())
+        J= int(oldpeak.get())
+    except:
+        messagebox.showerror("missing data","Few missing data entry!!")  
+        return
+    
+
+    print("A is age: ",A )
+    print("B is gender: ",B )
+    print("C is cp: ",C)
+    print("D is trestbps",D )
+    print("E is chol ",E )
+    print("F is fbs: ",F )
+    print("G is restecg: ",G )
+    print("H is thalach ",H )
+    print("I is Exang",I )
+    print("J is oldpeak: ",J )
+    print("K is slop: ",K )
+    print("L is ca :",L )
+    print("M is thal :",M )
+
+    # First Graph
+    f=Figure(figsize=(5,5),dpi=100)
+    a=f.add_subplot(111)
+    a.plot(["Sex","fbs","exang"],[B,F,I])
+    canvas = FigureCanvasTkAgg(f)
+    canvas.get_tk_widget().pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+    canvas._tkcanvas.place(width=250,height=250,x=600,y=240)
+
+    # Second Graph
+    f2=Figure(figsize=(5,5),dpi=100)
+    a2=f2.add_subplot(111)
+    a2.plot(["age","trestbps","chol","thalach"],[A,D,E,H])
+    canvas2 = FigureCanvasTkAgg(f2)
+    canvas2.get_tk_widget().pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+    canvas2._tkcanvas.place(width=250,height=250,x=860,y=240)
+
+    # Third Graph
+    f3=Figure(figsize=(5,5),dpi=100)
+    a3=f3.add_subplot(111)
+    a3.plot(["oldpeak","restecg","cp"],[J,G,C])
+    canvas3 = FigureCanvasTkAgg(f3)
+    canvas3.get_tk_widget().pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+    canvas3._tkcanvas.place(width=250,height=250,x=600,y=470)
+
+    # Fourth Graph
+    f4=Figure(figsize=(5,5),dpi=100)
+    a4=f4.add_subplot(111)
+    a4.plot(["slope","ca","thal"],[K,L,M])
+    canvas4 = FigureCanvasTkAgg(f4)
+    canvas4.get_tk_widget().pack(side=tk.BOTTOM,fill=tk.BOTH,expand=True)
+    canvas4._tkcanvas.place(width=250,height=250,x=860,y=470)
+    
+    # Input data
+    input_data=(A,B,C,D,E,F,G,H,I,J,K,L,M)
+
+    input_data_as_numpy_array=np.asanyarray(input_data)
+
+    # Reshape the numpy array as we are predicting for only on instance
+    input_data_reshape = input_data_as_numpy_array.reshape[1,-1]
+
+    prediction = model.predict(input_data_reshape) 
+    print(prediction[0])
+
+    if (prediction[0]==0):
+        print('The person does not have a heart disease')
+        report.config(text=f"Report:{0}",fg="#8dc63f")
+        report1.config(text=f"{name},you do not have a heart disease")
+
+    else:
+        print('The person has a heart disease')
+        report.config(text=f"Report:{1}",fg="#ed3c24")
+        report1.config(text=f"{name},you do not have a heart disease")
+
+
+
+
+    
+  
+        
+    
+
+      
+
+############################################## info window (operated by info button) ############################################################
+def Info():
+
+
+    Icon_window=Toplevel(root)
+    Icon_window.title("Info")
+    Icon_window.geometry("700x600+400+100")
+
+    #icon_image
+    icon_image=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/info.png")
+    Icon_window.iconphoto(False,icon_image)
+
+    #heading
+    Label(Icon_window,text="Information Related to dataset",font="robot 19 bold").pack(padx=20,pady=20)
+
+    #info
+    Label(Icon_window,text="age - age in years",font="arial 11").place(x=20,y=100)
+    Label(Icon_window,text="sex - sex (1 = male; 0 = female)",font="arial 11").place(x=20,y=130)
+    Label(Icon_window,text="cp - chest pain type (0 = typical angina; 1 = atypical angina; 2 = non-anginal pain; 3 = asymptomatic)",font="arial 11").place(x=20,y=160)
+    Label(Icon_window,text="trestbps - resting blood pressure (in mm Hg on admission to the hospital)",font="arial 11").place(x=20,y=190)
+    Label(Icon_window,text="chol - serum cholestoral in mg/dl",font="arial 11").place(x=20,y=220)
+    Label(Icon_window,text="fbs - fasting blood sugar > 120 mg/dl (1 = true; 0 = false)",font="arial 11").place(x=20,y=250)
+    Label(Icon_window,text="restecg - resting electrocardiographic results (0 = normal; 1 = having ST-T; 2 = hypertrophy)",font="arial 11").place(x=20,y=280)
+    Label(Icon_window,text="thalach - maximum heart rate achieved",font="arial 11").place(x=20,y=310)
+    Label(Icon_window,text="exang - exercise induced angina (1 = yes; 0 = no)",font="arial 11").place(x=20,y=340)
+    Label(Icon_window,text="oldpeak - ST depression induced by exercise relative to rest",font="arial 11").place(x=20,y=370)
+    Label(Icon_window,text="slope - the slope of the peak exercise ST segment (0 = upsloping; 1 = flat; 2 = downsloping)",font="arial 11").place(x=20,y=400)
+    Label(Icon_window,text="ca - number of major vessels (0-3) colored by flourosopy",font="arial 11").place(x=20,y=430)
+    Label(Icon_window,text="thal - 0 = normal; 1 = fixed defect; 2 = reversable defect",font="arial 11").place(x=20,y=460)
+
+
+
+
+
+    Icon_window.mainloop()
+
+
+#It is used for closing window
+def logout():
+    root.destroy()   
+
+
+# Clear (with the help of clear we can clear more entry field in once)
+def Clear():
+    Name.get('')
+    DOB.get('')
+    trestbps.get('')
+    chol.get('')
+    thalach.set('')
+    oldpeak.set('')
+
 ################################################################################################################################################
 
 #ICON 1
-image_icon=PhotoImage(file="Images/icon.png")
+image_icon=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/icon.png")
 root.iconphoto(False,image_icon)
 
 #Header Section 2
-logo=PhotoImage(file="Images/header.png")
+logo=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/header.png")
 myimage=Label(image=logo,bg=background)
 myimage.place(x=0,y=0)
 
@@ -48,8 +245,8 @@ Label(Heading_entry,text="Date",font="arial 13",bg="#df2d4b",fg=framebg).place(x
 Label(Heading_entry,text="Patient Name",font="arial 13",bg="#df2d4b",fg=framebg).place(x=30,y=90)
 Label(Heading_entry,text="Birth Year",font="arial 13",bg="#df2d4b",fg=framebg).place(x=430,y=90)
 
-Entry_image=PhotoImage(file="Images/Rounded Rectangle 1.png")
-Entry_image2=PhotoImage(file="Images/Rounded Rectangle 2.png")
+Entry_image=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/Rounded Rectangle 1.png")
+Entry_image2=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/Rounded Rectangle 2.png")
 Label(Heading_entry,image=Entry_image,bg="#df2d4b").place(x=20,y=30)
 Label(Heading_entry,image=Entry_image,bg="#df2d4b").place(x=430,y=30)
 
@@ -91,7 +288,7 @@ def selection():
         return(Gender)
         print(Gender)
 
-    elif gen.get==2:
+    elif gen.get()==2:
         Gender=0
         return(Gender)
         print(Gender) 
@@ -108,7 +305,7 @@ def selection2():
         return(Fbs)
         print(Fbs)
 
-    elif fbs.get==2:
+    elif fbs.get()==2:
         Fbs=0
         return(Fbs)
         print(Fbs) 
@@ -123,11 +320,10 @@ def selection3():
         return(Exang)
         print(Exang)
 
-    elif exang.get==2:
+    elif exang.get()==2:
         Exang=0
         return(Exang)
         print(Exang) 
-
 
     else:
         print(Exang)  
@@ -163,14 +359,139 @@ Label(Detail_entry,text="ca:",font="arial 13 ",bg=framebg,fg=framefg).place(x=10
 Label(Detail_entry,text="thal:",font="arial 13 ",bg=framebg,fg=framefg).place(x=10,y=210)
 
 
-cp_combobox=Combobox(Detail_entry,values=[],font="arial 12" ,state="r",width=11)
+def selection4():
+   input=cp_combobox.get()
+   if input=="0 = typical angina":
+       return(0)
+   elif input=="1 = atypical angina":
+       return(1)
+   elif input=="2 = non-anginal pain":
+       return(2)
+   elif input=="3 =asymptomatic":
+       return(3)
+   else:
+       print(Exang)
+   
+
+
+
+
+def selection5():
+
+    input=slope_combobox.get()
+    if input == "0 = upsloping":
+        return(0)
+    elif input == "1 = flat":
+        return(1)
+    elif input == "2 = downsloping":
+        return(2)
+   
+    else:
+        print(Exang)        
+
+
+cp_combobox=Combobox(Detail_entry,values=['0 = typical angina','1 = atypical angina','2 = non-anginal pain','3 =asymptomatic'],font="arial 12" ,state="r",width=14)
+restecg_combobox=Combobox(Detail_entry,values=['0 ','1','2'],font="arial 12" ,state="r",width=11)
+slope_combobox=Combobox(Detail_entry,values=['0 = upsloping','1 = flat','2 = downsloping'],font="arial 12" ,state="r",width=12)
+ca_combobox=Combobox(Detail_entry,values=['0','1','2','3','4'],font="arial 12" ,state="r",width=14)
+thal_combobox=Combobox(Detail_entry,values=['0','1','2','3'],font="arial 12" ,state="r",width=14)
 
 
 cp_combobox.place(x=50,y=50)
+restecg_combobox.place(x=80,y=90)
+slope_combobox.place(x=70,y=130)
+ca_combobox.place(x=50,y=170)
+thal_combobox.place(x=50,y=210)
+
+
+######################################### Data Entry box ####################################################
+Label(Detail_entry,text="Smoking:",font="arial 13",width=7,bg="#dbe0e3",fg="black").place(x=240,y=50)
+Label(Detail_entry,text="trestbps:",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=90)
+Label(Detail_entry,text="chol:",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=130)
+Label(Detail_entry,text="thalach:",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=170)
+Label(Detail_entry,text="oldpeak:",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=210)
+
+trestbps=StringVar()
+chol=StringVar()
+thalach=StringVar()
+oldpeak=StringVar()
+
+trestbps_entry = Entry(Detail_entry,textvariable=trestbps,width=10,font="arial 15",bg="#ededed",fg="#222222",bd=0)
+chol_entry = Entry(Detail_entry,textvariable=chol,width=10,font="arial 15",bg="#ededed",fg="#222222",bd=0)
+thalach_entry = Entry(Detail_entry,textvariable=thalach,width=10,font="arial 15",bg="#ededed",fg="#222222",bd=0)
+oldpeak_entry = Entry(Detail_entry,textvariable=oldpeak,width=10,font="arial 15",bg="#ededed",fg="#222222",bd=0)
+
+
+trestbps_entry.place(x=320,y=90)
+chol_entry.place(x=320,y=130)
+thalach_entry.place(x=320,y=170)
+oldpeak_entry.place(x=320,y=210)
+
+
+####################################### <Report> ###########################################################
+square_report_image=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/Report.png")
+report_background=Label(image=square_report_image,bg=background)
+report_background.place(x=1120,y=340)
+
+report=Label(root,font="arial 25 bold",bg="white",fg="#8dc63f")
+report.place(x=1170,y=550)
+
+report1=Label(root,font="arial 10 bold",bg="white")
+report1.place(x=1130,y=610)
+
+################################################ Graph ##############################################################
+graph_image=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/graph.png")
+Label(image=graph_image).place(x=600,y=270)
+Label(image=graph_image).place(x=860,y=270)
+Label(image=graph_image).place(x=600,y=500)
+Label(image=graph_image).place(x=860,y=500)
+
+################################################ Button ##############################################################
+analysis_button=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/Analysis.png")
+Button(root,image=analysis_button,bd=0,bg=background,cursor='hand2',command=analysis).place(x=1130,y=240)
+
+################################################## Info button ########################################################
+info_button=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/info.png")
+Button(root,image=info_button,bd=0,bg=background,cursor='hand2',command=Info).place(x=10,y=240)
+
+
+################################################## Save button ########################################################
+save_button=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/save.png")
+Button(root,image=save_button,bd=0,bg=background,cursor='hand2').place(x=1370,y=250)
+
+##################################### smoking and non smoking button ###########################################################
+
+button_mode=True
+choice="smoking"
+
+def changemode():
+    global button_mode
+    global choice
+
+
+    if button_mode:
+        choice="non_smoking"
+        mode.config(image=non_smoking_icon,activebackground="white")
+        button_mode=False
+    else:
+        choice="smoking"
+        mode.config(image=smoking_icon,activebackground="white")
+        button_mode=True
+
+    print(choice)    
 
 
 
+smoking_icon=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/smoker.png")
+non_smoking_icon=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/non-smoker.png")
 
+mode=Button(root,image=smoking_icon,bg="#dbe0e3",bd=0,cursor="hand2",command=changemode)
+mode.place(x=350,y=495)
 
+################################################ Logout Button ##############################################################
+
+logout_icon=PhotoImage(file="Heart_Attack_Prediction-using-Machine-Learning/Images/logout.png")
+logout_button=Button(root,image=logout_icon,bg="#df2d4b",cursor="hand2",bd=0,command=logout)
+logout_button.place(x=1390,y=60)
 
 root.mainloop()
